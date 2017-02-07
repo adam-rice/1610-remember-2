28b1dee5c5e2a8632c4e9f2bf9674733bbb61904
Dylan Avery <fossilfuelmusic@gmail.com>
2017-02-07 15:37:23 -0700

    render form onto dom as child of reminders list, need to make a model for reminder

---
+++ app/components/add-reminder.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  store: Ember.inject.service(),
+
+
+  tagName: 'section',
+  classNames: ['add-reminder'],
+
+  text: '',
+
+
+  actions: {
+    createReminder() {
+      const reminder = this.getProperties('text')
+      this.get('store').createRecord('reminder', reminder).save().then(() => {
+        this.setProperties({ text: ''})
+      })
+    }
+  }
+});
