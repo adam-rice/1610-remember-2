81ad948f7a90c187f9a8969801eaf8d3c7fb7c41
Dylan Avery <fossilfuelmusic@gmail.com>
2017-02-08 16:40:56 -0700

    Add active class CSS

--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('reminders', { path: '/' })
+  this.route('reminder', {path: 'reminders/:reminder_id'});
 });
 
 export default Router;
