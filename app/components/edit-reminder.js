48b067b414edcce4d12a3fe201b25b1f95287a03
Adam Rice <adamcoltonrice@gmail.com>
2017-02-13 13:53:34 -0700

    Correct routes action name on edit & set date for edit reminder default

--- app/components/edit-reminder.js
+++ app/components/edit-reminder.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     editReminder(model) {
-      //date
+      model.date = model.date || new Date();
       model.save();
       this.sendAction();
     }
--- app/routes/reminders/reminder/edit.js
+++ app/routes/reminders/reminder/edit.js
@@ -1,11 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model(params) {
-    return this.get('store').find('reminder', params.reminder_id);
-  },
   actions: {
-    saveReminder() {
+    editReminder() {
       this.transitionTo('reminders');
     }
   }
