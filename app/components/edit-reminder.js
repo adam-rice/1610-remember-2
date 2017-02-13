e0d08919127e6bfb540788161ad00b986c619804
Adam Rice <adamcoltonrice@gmail.com>
2017-02-13 13:40:38 -0700

    Connect edit reminder handlebar to action & add transition to on submit

--- app/components/edit-reminder.js
+++ app/components/edit-reminder.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    editReminder(model) {
+      //date
+      model.save();
+      this.sendAction();
+    }
+  }
 });
