47418b7e1d25e3d46b02edf65dad068b20603bcf
Adam Rice <adamcoltonrice@gmail.com>
2017-02-14 14:31:28 -0700

    Add rollback changes action and refactor hbs

--- app/components/edit-reminder.js
+++ app/components/edit-reminder.js
@@ -6,6 +6,12 @@ export default Ember.Component.extend({
       model.date = model.date || new Date();
       model.save();
       this.sendAction();
+    },
+    rollbackChanges() {
+      // if(model.get('hasDirtyAttributes')) {
+      //   model.rollbackChanges();
+      // }
+      console.log('boom');
     }
   }
 });
